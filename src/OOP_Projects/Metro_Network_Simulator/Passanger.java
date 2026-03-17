e66a52fec339974146542446844579c30364956b
Attila Fekete <feketeattila777@proton.me>
2026-03-17 16:22:02 +0100

    MetroNetwork practice

---
+++ src/OOP_Projects/Metro_Network_Simulator/Passanger.java
@@ -0,0 +1,4 @@
+package OOP_Projects.Metro_Network_Simulator;
+
+public class Passanger {
+}
---
+++ src/OOP_Projects/Metro_Network_Simulator/Station.java
@@ -0,0 +1,33 @@
+package OOP_Projects.Metro_Network_Simulator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Station {
+
+    private String name;
+    private Set<Line> lines = new HashSet<>();
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addLine(Line line) {
+        lines.add(line);
+    }
+
+    public Set<Line> getLines() {
+        return lines;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name);
+        return sb.toString();
+    }
+}
