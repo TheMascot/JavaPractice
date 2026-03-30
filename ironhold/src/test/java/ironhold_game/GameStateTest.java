bd967de326299e853a037bfc2a5a69f1b82bbb00
Attila Fekete <feketeattila777@proton.me>
2026-03-30 14:16:40 +0200

    testing setup

---
+++ ironhold/src/test/java/ironhold_game/GameStateTest.java
@@ -0,0 +1,10 @@
+package ironhold_game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameStateTest {
+
+    @org.junit.jupiter.api.Test
+    void test_InitializeStockpile() {
+    }
+}
